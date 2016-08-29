3468088863cfdcf13d1b3c68e5c05796ed1e5093
Ron Matsliah <ronma@perfectomobile.com>
2016-08-29 16:07:06 +0300

    added elements test

--- Optimizer-Dom/src/test/java/com/perfecto/beat/OptimizerElementFind.java
+++ Optimizer-Dom/src/test/java/com/perfecto/beat/OptimizerElementFind.java
@@ -197,19 +197,3 @@ public class OptimizerElementFind {
     }
 }
 
-class OptimizerObject {
-
-    //select country button
-    final static String selectCountry = "pm-select-country-label"; // by class
-
-    final static String closeButton = "//*[text()='Close']/parent::*"; // xpath
-
-    final static String FullResultBtn = ".//*[@id='showFullResults']"; // xpath
-
-    final static String FullResultPage = "//*[text()='TEST COVERAGE OPTIMIZER']"; // xpath
-
-    final static String typeSelect = "body > div.pm-content-container.ng-scope.results-page > " +
-            "filters-container > div > device-filter > div > div.pm-selection-container.pm-device-type-container > device-selector:nth-child(1) > div > img"; // css
-
-    final static String fullPicButton = "showFullResults"; // by id
-}
